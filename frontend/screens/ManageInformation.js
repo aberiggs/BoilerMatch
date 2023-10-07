f34d10fb3966ecc6ea884d8591f7ec7a28308c3a
troytam <tstamura@purdue.edu>
2023-10-07 13:00:59 -0400

    Done with actual information input

--- frontend/screens/ManageInformation.js
+++ frontend/screens/ManageInformation.js
@@ -10,6 +10,7 @@ export default function ManageInformation() {
   const [lastName, setLastName] = useState('');
   const [major, setMajor] = useState('');
   const [graduation, setGraduation] = useState('');
+  const [pet, setPet] = useState('');
 
   const handleFirstNameChange = (text) => {
     setFirstName(text);
@@ -23,11 +24,14 @@ export default function ManageInformation() {
   const handleMajorChange = (text) => {
     setMajor(text);
   };
+  const handlePetChange = (text) => {
+    setPet(text);
+  };
   const handleSubmit = () => {
     console.log("Submit pressed");
   }
   
-  
+
   return (
     <View style={styles.container}>
 
@@ -115,10 +119,130 @@ export default function ManageInformation() {
           onChangeText={handleMajorChange}
         />
 
-          <TouchableOpacity style={styles.button} onPress={handleSubmit}>
-          <Text style={styles.buttonText}>Submit Information</Text>
-          </TouchableOpacity>
-        </ScrollView>
+        <Text style={styles.subtitle}>Select Race</Text>
+
+        <RNPickerSelect
+          placeholder={ {label: "Select Race", value: null}}
+          onValueChange={(value) => console.log(value)}
+          items={[
+              { label: "American Indian or Alaskan Native", value: "indian" },
+              { label: "Asian", value: "asian" },
+              { label: "Black or African Descent", value: "black" },
+              { label: "Hispanic or Latino", value: "hispanic" },
+              { label: "Native Hawaiian or Pacific Islander", value: "islander" },
+              { label: "White", value: "white" },
+              { label: "Other", value: "other" },
+              { label: "Prefer not to say", value: "preferNotToSay" },
+              { label: "Two or more races", value: "twoOrMore" },
+          ]}
+          style={pickerSelectStyles}
+        /> 
+
+        <Text style={styles.subtitle}>Select Pets</Text>
+
+        <RNPickerSelect
+          placeholder={ {label: "Select Pets", value: null}}
+          onValueChange={(value) => console.log(value)}
+          items={[
+              { label: "Dog", value: "dog" },
+              { label: "Cat", value: "cat" },
+              { label: "Reptile", value: "reptile" },
+              { label: "Amphibian", value: "amphibian" },
+              { label: "Bird", value: "bird" },
+              { label: "Fish", value: "fish" },
+              { label: "Hamster", value: "hamster" },
+              { label: "Rabbit", value: "rabbit" },
+              { label: "Other", value: "other" },
+              { label: "Pet-free", value: "pet-free" },
+              { label: "Allergic to pets", value: "allergic" },
+          ]}
+          style={pickerSelectStyles}
+        /> 
+
+        <Text style={styles.subtitle}>Select Religion</Text>
+
+        <RNPickerSelect
+          placeholder={ {label: "Select Religion", value: null}}
+          onValueChange={(value) => console.log(value)}
+          items={[
+              { label: "Agnostic", value: "agnostic" },
+              { label: "Atheist", value: "atheist" },
+              { label: "Buddhist", value: "buddhist" },
+              { label: "Catholic", value: "catholic" },
+              { label: "Christian", value: "christian" },
+              { label: "Hindu", value: "hindu" },
+              { label: "Jewish", value: "jewish" },
+              { label: "Muslim", value: "muslim" },
+              { label: "Sikh", value: "sikh" },
+              { label: "Spiritual", value: "spiritual" },
+              { label: "Other", value: "other" },
+          ]}
+          style={pickerSelectStyles}
+        /> 
+
+        <Text style={styles.subtitle}>Select Political Views</Text>
+
+        <RNPickerSelect
+          placeholder={ {label: "Select Political Views", value: null}}
+          onValueChange={(value) => console.log(value)}
+          items={[
+              { label: "Conservative", value: "conservative" },
+              { label: "Liberal", value: "liberal" },
+              { label: "Moderate", value: "moderate" },
+              { label: "Not Political", value: "not political" },
+              { label: "Other", value: "other" },
+              { label: "Prefer not to say", value: "preferNotToSay" },
+          ]}
+          style={pickerSelectStyles}
+        /> 
+
+        <Text style={styles.subtitle}>Select Sleeping Habits</Text>
+
+        <RNPickerSelect
+          placeholder={ {label: "Select Sleeping Habits", value: null}}
+          onValueChange={(value) => console.log(value)}
+          items={[
+              { label: "Early bird", value: "earlyBird" },
+              { label: "Night owl", value: "nightOwl" },
+              { label: "In a spectrum", value: "inSpectrum" },
+          ]}
+          style={pickerSelectStyles}
+        />
+
+        <Text style={styles.subtitle}>Select Drinking Habits</Text>
+
+        <RNPickerSelect
+          placeholder={ {label: "Select Drinking Habits", value: null}}
+          onValueChange={(value) => console.log(value)}
+          items={[
+              { label: "Not for me", value: "notForMe" },
+              { label: "Sober", value: "sober" },
+              { label: "On special occasions", value: "specialOccasion" },
+              { label: "Socially on weekends", value: "sociallyWeekends" },
+              { label: "Most nights", value: "mostNights" },
+          ]}
+          style={pickerSelectStyles}
+        /> 
+
+        {/* <Text style={styles.subtitle}>Select Study Habits - Environment</Text>
+
+        <RNPickerSelect
+          placeholder={ {label: "Select study habits", value: null}}
+          onValueChange={(value) => console.log(value)}
+          items={[
+              { label: "Prefer silence", value: "silence" },
+              { label: "Music/background noise is okay", value: "noiseOk" },
+              { label: "Flexible, can adapt to different environments", value: "adaptable" },
+          ]}
+          style={pickerSelectStyles}
+        />  */}
+
+
+        <TouchableOpacity style={styles.button} onPress={handleSubmit}>
+        <Text style={styles.buttonText}>Submit Information</Text>
+        </TouchableOpacity>
+
+      </ScrollView>
     </View>
 
     
