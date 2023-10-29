df38df5b605dcd56bcdca11b58bbd6c579dd0afd
aberiggs <sprocket710@gmail.com>
2023-10-29 16:46:59 -0400

    Carousel setup

--- frontend/screens/MainFeed/UserProfile.js
+++ frontend/screens/MainFeed/UserProfile.js
@@ -1,10 +1,43 @@
 import { StyleSheet, Text, View, Pressable, ScrollView } from 'react-native';
 import React from 'react';
 import { Avatar } from '@rneui/themed';
+import Carousel from 'react-native-snap-carousel';
+
+
+const carouselItems = [
+  {
+      title:"Item 1",
+      text: "Text 1",
+  },
+  {
+      title:"Item 2",
+      text: "Text 2",
+  },
+  {
+      title:"Item 3",
+      text: "Text 3",
+  },
+  {
+      title:"Item 4",
+      text: "Text 4",
+  },
+  {
+      title:"Item 5",
+      text: "Text 5",
+  },
+]
 
 export default function userProfile(props) {
   
   const selectedUser = props.user
+
+  _renderItem = ({item, index}) => {
+    return (
+        <View style={styles.slide}>
+            <Text style={styles.title}>{ item.title }</Text>
+        </View>
+    );
+}
 
   return (
     <View style={modalStyles.modalContainer}>
@@ -17,6 +50,16 @@ export default function userProfile(props) {
             containerStyle={{backgroundColor: 'grey', margin: 10, alignSelf: 'center'}}
             activeOpacity={0.8}
           />
+
+          <Carousel
+            ref={(c) => { this._carousel = c; }}
+            data={carouselItems}
+            renderItem={this._renderItem}
+            sliderWidth={300}
+            itemWidth={300}
+          />
+
+
           <Text style={styles.subtitle}>Name: {selectedUser.information.firstName} {selectedUser.information.lastName}</Text>
           <Text style={styles.subtitle}>Gender: {selectedUser.information.gender}</Text>
           <Text style={styles.subtitle}>Grad Year: {selectedUser.information.graduation}</Text>
