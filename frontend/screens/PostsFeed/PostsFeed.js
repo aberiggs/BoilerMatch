b4cdba61428a51156d8f4249da9c187680ae1768
troytam <tstamura@purdue.edu>
2023-11-13 19:38:17 -0500

    UI for posts

--- frontend/screens/PostsFeed/PostsFeed.js
+++ frontend/screens/PostsFeed/PostsFeed.js
@@ -1,15 +1,18 @@
 import { StyleSheet, View, FlatList, Text, TouchableOpacity} from 'react-native';
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
 import { Ionicons } from '@expo/vector-icons';
 import axios from "axios"
 import * as SecureStore from 'expo-secure-store';
+import themeContext from '../../theme/themeContext';
 
 import CreatePostModal from './CreatePostModal'; // Import the ReportBlockModal component
 
 export default function PostsFeed({navigation}) {
 
     const [createPostModalVisible, setCreatePostModalVisible] = useState(false);
-
+    const theme = useContext(themeContext);
+    const [username, setUsername] = useState(null);
+    
     const openCreatePostModal = () => {
         setCreatePostModalVisible(true);
     };
@@ -18,16 +21,34 @@ export default function PostsFeed({navigation}) {
         setCreatePostModalVisible(false);
     };
 
+    const initialize = async () => {
+      const userVal = await SecureStore.getItemAsync('username')
+      setUsername(userVal)
+    }
+
+    initialize()
+
+    // @ Sprocket, you can look at ChatList and formatTimestamp or formatTimestampDate
+    // and see how you wanna format the timestamp thats going on the feed
+    
+
     const displayedPosts = [{title: "Looking for a roommate!"},
                             {title: "Subleasing for Spring 2024"}, 
                             {title: "Where can I find cheaper leases?"}]
 
     const PostItem = ({item}) => (
-        <View style={{height: 20, width: "full"}}>
-            <Text>
-                {item.title}
+      <View style={[styles.feedItem, {backgroundColor:theme.background}]}>
+          <View style={[feedStyles.infoContainer, {backgroundColor:theme.backgroundColor}]}>
+            <Text style={[feedStyles.name, {color:theme.color}]}>{"Title"}</Text>
+            <Text style={feedStyles.username}>@{"username handle"}</Text>
+            <Text style={[styles.subtitle, {color:theme.color}]}>
+              <Text style={[feedStyles.infoLabel, {color:theme.color}]}>Timestamp: </Text>
+            </Text>
+            <Text style={[styles.subtitle, {color:theme.color}]}>
+              <Text style={[feedStyles.infoLabel, {color:theme.color}]}>Details </Text>
             </Text>
             
+          </View> 
         </View>
     )
 
@@ -53,7 +74,6 @@ export default function PostsFeed({navigation}) {
     )
 
 }
-
 
 const styles = StyleSheet.create({
     container: {
@@ -85,4 +105,39 @@ const styles = StyleSheet.create({
         fontSize: 16,
         alignSelf: "center"
     },
+    feedItem: {
+      // Take up the entire available space
+       backgroundColor: 'white',
+       alignContent: 'center',
+       padding: 15,
+       marginBottom: 10,
+       shadowColor: 'black',
+       shadowOffset: { width: 0, height: 2 },
+       shadowOpacity: 0.2,
+       shadowRadius: 2,
+       elevation: 2,
+     },
+})
+
+const feedStyles = StyleSheet.create({
+  iconContainer: {
+    paddingHorizontal: 5
+  },
+  infoContainer: {
+    width: '100%',
+    padding: 10,
+    fontSize: 16,
+    lineHeight: 40,
+  },
+  name: {
+    fontSize: 25
+  },
+  username: {
+    fontSize: 16,
+    color: 'grey',
+    paddingBottom: 6
+  },
+  infoLabel: {
+    fontWeight: '600',
+  },  
 })
